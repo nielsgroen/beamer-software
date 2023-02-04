68710df90cd2264d9c3b225057cb7f87ecd82b10
Niels <nielsgroeneveld8@gmail.com>
2023-02-04 21:22:37 +0100

    tauri vue setup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,11 @@
 #![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
 )]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+  format!("Hello, {}!", name)
 }
 
 fn main() {
